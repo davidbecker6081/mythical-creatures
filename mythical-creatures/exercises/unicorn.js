1dab4bd130e6afc7462d3831fea60be48eae337f
David Becker <david@beckersite.com>
2017-12-17 17:41:50 -0700

    Finish Unicorn

--- mythical-creatures/exercises/unicorn.js
+++ mythical-creatures/exercises/unicorn.js
@@ -1,3 +1,9 @@
-function Unicorn() {}
+function Unicorn(name, color = 'white') {
+  this.name = name;
+  this.color = color;
+}
+
+Unicorn.prototype.isWhite = () => this.color === 'white';
+Unicorn.prototype.says = (string) => `**;* ${string} *;**`;
 
 module.exports = Unicorn;
