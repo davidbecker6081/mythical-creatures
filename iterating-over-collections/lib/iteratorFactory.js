f1ee230a5c79130132175659f38cf6fafb1b4fde
David Becker <david@beckersite.com>
2017-12-29 19:35:18 -0700

    Finish one-test

--- iterating-over-collections/lib/iteratorFactory.js
+++ iterating-over-collections/lib/iteratorFactory.js
@@ -2,4 +2,42 @@ function iteratorFactory() {
 
 };
 
+iteratorFactory.prototype.sum = function(arr) {
+  if (typeof(arr[0]) === 'string') {
+    return arr.reduce((string, additive) => {
+      string += additive
+      return string
+    }, '')
+  } else {
+    return arr.reduce((sum, additive) => {
+      sum += additive
+      return sum
+    }, 0)
+  }
+};
+
+iteratorFactory.prototype.sumAndSquare = function(arr) {
+  const sum = arr.reduce((sum, additive) => {
+    sum += additive
+    return sum
+  }, 0)
+
+  return sum * sum
+};
+
+iteratorFactory.prototype.onlyEvens = function(arr) {
+  return arr.filter(num => num % 2 === 0)
+};
+
+iteratorFactory.prototype.onlyOdds = function(arr) {
+  return arr.filter(num => num % 2 !== 0)
+};
+
+iteratorFactory.prototype.squareAndSum = function(arr) {
+  return arr.map(num => num * num).reduce((sum, num) => {
+    sum += num
+    return sum
+  }, 0)
+};
+
 module.exports = iteratorFactory;
--- iterating-over-collections/test/one-test.js
+++ iterating-over-collections/test/one-test.js
@@ -9,13 +9,13 @@ describe('iteratorFactory', function() {
     assert.equal(typeof factory, "object");
   });
 
-  it.skip('has a prototype function called `sum`', function() {
+  it('has a prototype function called `sum`', function() {
     var factory = new iteratorFactory();
 
     assert.equal(typeof factory.sum, 'function');
   });
 
-  it.skip('can sum a collection of integers', function() {
+  it('can sum a collection of integers', function() {
     var factory = new iteratorFactory();
 
     assert.equal(factory.sum([1, 2, 3, 4, 5]), 15);
@@ -23,7 +23,7 @@ describe('iteratorFactory', function() {
     assert.equal(factory.sum([10, 20, 30, 40, 50]), 150);
   });
 
-  it.skip('can sum a collection of strings', function() {
+  it('can sum a collection of strings', function() {
     var factory = new iteratorFactory();
 
     assert.equal(factory.sum(["one, ", "two, ", "three"]), "one, two, three");
@@ -31,7 +31,7 @@ describe('iteratorFactory', function() {
     assert.equal(factory.sum(["hey, ", "what's ", "up", "?"]), "hey, what's up?");
   });
 
-  it.skip('can sum a collection of integers and then square the sum', function() {
+  it('can sum a collection of integers and then square the sum', function() {
     var factory = new iteratorFactory();
 
     assert.equal(factory.sumAndSquare([1, 2, 3, 4, 5]), 225);
@@ -39,23 +39,23 @@ describe('iteratorFactory', function() {
     assert.equal(factory.sumAndSquare([10, 11, 12, 13, 14]), 3600);
   })
 
-  it.skip('can filter out odd numbers from a collection of integers', function() {
+  it('can filter out odd numbers from a collection of integers', function() {
     var factory = new iteratorFactory();
 
-    assert.equal(factory.onlyEvens([1, 2, 3, 4, 5, 6]), [2, 4, 6]);
+    assert.deepEqual(factory.onlyEvens([1, 2, 3, 4, 5, 6]), [2, 4, 6]);
 
-    assert.equal(factory.onlyEvens([7, 8, 9, 10, 11, 12]), [8, 10, 12]);
+    assert.deepEqual(factory.onlyEvens([7, 8, 9, 10, 11, 12]), [8, 10, 12]);
   });
 
-  it.skip('can filter out even numbers from a collection of integers', function() {
+  it('can filter out even numbers from a collection of integers', function() {
     var factory = new iteratorFactory();
 
-    assert.equal(factory.onlyOdds([1, 2, 3, 4, 5, 6]), [1, 3, 5]);
+    assert.deepEqual(factory.onlyOdds([1, 2, 3, 4, 5, 6]), [1, 3, 5]);
 
-    assert.equal(factory.onlyOdds([7, 8, 9, 10, 11, 12]), [7, 9, 11]);
+    assert.deepEqual(factory.onlyOdds([7, 8, 9, 10, 11, 12]), [7, 9, 11]);
   });
 
-  it.skip('can square every number in a collection and then summing them', function() {
+  it('can square every number in a collection and then summing them', function() {
     var factory = new iteratorFactory();
 
     assert.equal(factory.squareAndSum([1, 2, 3, 4, 5, 6]), 91);
